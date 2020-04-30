ccb2d4c9d20de311db59828e408ff11ca4a1d536
Stacey Teltser <stacey.teltser@gmail.com>
2020-04-30 13:53:58 -0500

    injected 2 variables into the string

--- Decisions/Program.cs
+++ Decisions/Program.cs
@@ -38,9 +38,7 @@ namespace Decisions
 
             string message = (userValue == "1") ? "boat" : "strand of hair";
 
-            Console.Write("You won a ");
-            Console.Write(message);
-            Console.Write(".");
+            Console.WriteLine("You entered {0}, therefore you won a {1}.", userValue, message);
             Console.ReadLine();
         }
     }
